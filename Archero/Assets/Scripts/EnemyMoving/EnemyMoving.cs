414bb6822474e56630396fef350c876fe15e5e1d
Thắng Huỳnh Minh <luavang1509@gmail.com>
2020-08-15 22:42:57 +0700

    sua navmesh

---
+++ Archero/Assets/Scripts/EnemyMoving/EnemyMoving.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UIElements;
+
+public class EnemyMoving : MovingBase
+{
+    [SerializeField]
+    NavMeshAgent _navMeshAgent;
+    Transform playerTransform;
+    private void OnEnable()
+    {
+        playerTransform = GameManager.instance.player.transform;
+        _navMeshAgent.speed = maxSpeed;
+    }
+    public override void UpdateNormal()
+    {
+        direction = playerTransform.position - transform.position;
+        _navMeshAgent.SetDestination(playerTransform.position);
+    }
+}
+
