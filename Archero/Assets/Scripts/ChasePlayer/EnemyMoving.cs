f025c11c336f2faabf0b6362fab98e0f991581f1
Thắng Huỳnh Minh <luavang1509@gmail.com>
2020-08-15 15:55:24 +0700

    EnemyMoving (Navmesh)

---
+++ Archero/Assets/Scripts/ChasePlayer/EnemyMoving.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UIElements;
+
+public class EnemyMoving : MovingBase
+{
+    [SerializeField]
+    NavMeshAgent _navMeshAgent;
+    Transform playerTransform;
+    float attackRange = 2f;
+    private void OnEnable()
+    {
+        playerTransform = GameManager.instance.player.transform;
+    }
+    public override void UpdateNormal()
+    {
+        direction = playerTransform.position - transform.position;
+        if (direction.magnitude > attackRange)
+        {
+            _navMeshAgent.SetDestination(playerTransform.position);
+        }
+    }
+}
+
