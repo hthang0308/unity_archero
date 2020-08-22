f63df85a5c52f7b7f5b1e4b4610ead7df4715992
Thắng Huỳnh Minh <luavang1509@gmail.com>
2020-08-22 14:42:15 +0700

    Thêm Enemy Cannon, sửa map 4. Sẽ bổ sung explosion particle sau.

---
+++ Archero/Assets/Scripts/PlayingObject/Enemy/EnemyAttackStyle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttackStyle : AttackStyleBase<ExplosionDamageSource>
+{
+    float speed = 10f;
+    Transform player;
+    public override void OnEnable()
+    {
+        base.OnEnable();
+        player = GameManager.instance.player.transform;
+        countDownDelayNextAttack = delayNextAttack;
+    }
+    public override void UpdateNormal()
+    {
+        base.UpdateNormal();
+        transform.LookAt(player);
+    }
+    protected override void Attacking()
+    {
+        ExplosionDamageSource bullet = dmgPool.GetFromPool(dmgPrefab);
+        bullet.transform.SetPositionAndRotation(fireTransform.position, fireTransform.rotation);
+        bullet.bulletRig.velocity = speed * bullet.transform.forward;
+        isAttack = false;
+    }
+}
---
+++ Archero/Assets/Scripts/PlayingObject/Enemy/ExplosionDamageSource.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionDamageSource : DamageSourceBase
+{
+    public Rigidbody bulletRig;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 13)
+        {
+            DoDamage(other.GetComponent<LivingObjectInfo>());
+        }
+        gameObject.SetActive(false);
+    }
+}
